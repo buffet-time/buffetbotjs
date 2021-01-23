93190082df8083e0c4975e7165be1b849fafa4b4
Daniel Turcich <radd86@gmail.com>
2021-01-23 17:05:28 -0500

    Split out Acronym functions into it's own file, converted from require() to imports

---
+++ src/acronym.js
@@ -0,0 +1,40 @@
+import { wordsArray } from '../assets/words.js'
+
+export function acronym(word) {
+	const generatedAcronym = getWordsFromProvidedAcronym(word)
+		.toString()
+		.replace(/,/g, '  ')
+	if (generatedAcronym.length > 2000 || generatedAcronym === 'F') {
+		return "Yo gamer, that acronym was too larnge for discord. When's hotline?"
+	} else {
+		return generatedAcronym
+	}
+}
+
+function getWordsFromProvidedAcronym(acronym) {
+	const wordsFromAcronym = []
+	const acronymArray = acronym.toLowerCase().split('')
+	let lengthOfWords = 0
+
+	acronymArray.forEach((letter) => {
+		if (lengthOfWords > 2000) {
+			return 'F'
+		} else {
+			const wordsArray = getWordsStartingWith(letter)
+			const generatedWord =
+				wordsArray[Math.floor(Math.random() * wordsArray.length)]
+			wordsFromAcronym.push(generatedWord)
+			lengthOfWords += generatedWord.length + 1
+		}
+	})
+
+	return wordsFromAcronym
+}
+
+function getWordsStartingWith(letter) {
+	return wordsArray.filter((word) => {
+		if (word.startsWith(letter)) {
+			return word
+		}
+	})
+}
--- src/buffetBotMain.js
+++ src/buffetBotMain.js
@@ -1,8 +1,8 @@
-const Discord = require('discord.js')
-const config = require('../config.json')
-const wordsArray = require('../assets/words.json')
+import { Client } from 'discord.js'
+import config from '../config.js'
+import { acronym } from './acronym.js'
 
-const client = new Discord.Client()
+const client = new Client()
 let messageToSend = ''
 let command = ''
 let commandSender = ''
@@ -89,47 +89,3 @@ client.on('message', (message) => {
 })
 
 client.login(config.token)
-
-//////////////////////
-// Helper Functions //
-//////////////////////
-
-// Acronym functions
-function acronym(word) {
-	const generatedAcronym = getWordsFromProvidedAcronym(word)
-		.toString()
-		.replace(/,/g, '  ')
-	if (generatedAcronym.length > 2000 || generatedAcronym === 'F') {
-		return "Yo gamer, that acronym was too larnge for discord. When's hotline?"
-	} else {
-		return generatedAcronym
-	}
-}
-
-function getWordsFromProvidedAcronym(acronym) {
-	const wordsFromAcronym = []
-	const acronymArray = acronym.toLowerCase().split('')
-	let lengthOfWords = 0
-
-	acronymArray.forEach((letter) => {
-		if (lengthOfWords > 2000) {
-			return 'F'
-		} else {
-			const wordsArray = getWordsStartingWith(letter)
-			const generatedWord =
-				wordsArray[Math.floor(Math.random() * wordsArray.length)]
-			wordsFromAcronym.push(generatedWord)
-			lengthOfWords += generatedWord.length + 1
-		}
-	})
-
-	return wordsFromAcronym
-}
-
-function getWordsStartingWith(letter) {
-	return wordsArray.filter((word) => {
-		if (word.startsWith(letter)) {
-			return word
-		}
-	})
-}
