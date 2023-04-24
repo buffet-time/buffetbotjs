e60c37f520e4412945069b109c99358df7ea4018
Ubuntu <ubuntu@vps-923f922e.vps.ovh.ca>
2023-04-24 19:03:33 +0000

    upgraded packages

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
-export default {
-	token: 'discordTokenHere'
-}
+export const token =
+	'MzU3MjM2NTMxMDgzMDgzNzc4.Wbgscg.lxk5QLmlRy1BFevIBNpGoDq_QS8'
+export const rapidApiToken =
+	'58c8cf9028msh8db4a611372770cp17ea91jsn74f1cc4a62a7'
