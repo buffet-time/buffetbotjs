a4fecb34c5b8e43b7bcc7ad3ce1858a4da8c7fbe
Daniel Turcich <github@danielTurcich.com>
2023-09-10 02:48:01 -0400

    blargh

--- src/commands/audio/audioCommands.ts
+++ src/commands/audio/audioCommands.ts
@@ -1,7 +1,7 @@
 import { getVoiceConnection, joinVoiceChannel } from '@discordjs/voice'
 import type { VoiceChannel } from 'discord.js'
 import { readdir } from 'fs/promises'
-import { updateBotStatus } from 'src/main'
+import { updateBotStatus } from '../../main'
 import {
 	player,
 	saveYoutubeVideoToOgg,
