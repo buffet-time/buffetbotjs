545330a21cd51b258d5752110f8e1c9bdd9dc830
Daniel Turčić <github@danielTurcich.com>
2024-03-26 23:29:41 -0400

    Test branch (#10)
    * blargh
    
    * blargh
    
    * blargh it works, no comments now

--- src/commands/sheets.ts
+++ src/commands/sheets.ts
@@ -44,7 +44,7 @@ const sheetsCommand: Command = {
 				{ name: 'Music', value: 'Music' },
 				{ name: 'Games', value: 'Games' },
 				{ name: 'Movies', value: 'Movies' },
-				{ name: 'TV', value: 'tv' }
+				{ name: 'TV', value: 'TV' }
 			]
 		}
 	],
@@ -105,7 +105,7 @@ async function getNumberOfRows(
 ): Promise<number | undefined> {
 	try {
 		// TODO: cleanup hardcoded ports
-		return (await await ProperFetch(
+		return (await ProperFetch(
 			`${siteEndpoint}/Sheets?id=${id}&range=${range}&rows=true&nonmusic=true`
 		)) as number
 	} catch (error) {
@@ -120,7 +120,7 @@ async function getRowByIndex(
 	range: string
 ): Promise<string[] | undefined> {
 	try {
-		return (await await ProperFetch(
+		return (await ProperFetch(
 			`${siteEndpoint}/Sheets?id=${id}&range=${range}&index=${index}`
 		)) as string[]
 	} catch (error) {
